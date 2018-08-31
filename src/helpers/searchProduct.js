fd91ba42f4439c2fc886f9885f6064a9e0b921bd
phanna <studentphanna@gmail.com>
2018-08-31 12:01:41 +0700

    Change search to case insensitive and change container to startsWith

--- src/helpers/searchProduct.js
+++ src/helpers/searchProduct.js
@@ -1,10 +1,9 @@
 export async function searchProduct (key, searchIndex) {
-    const list = []
-    searchIndex.forEach((item) => {
-        if (item.name.includes(key)) {
-            list.push(item)
-        }
-    })
-
-    return list
+	let list = [];
+    list = searchIndex.filter(item => {
+        let search = item.name.toLowerCase();
+        return search.startsWith(key);
+    });
+    
+    return list;
 }
